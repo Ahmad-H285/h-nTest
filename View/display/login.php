0b9e86e198dafec6e156a1ccfdbf80d9b59bdc57
NourEldin275 <noureldin.ali.m@gmail.com>
2015-05-07 16:14:28 +0200

    Created a simple login page

---
+++ View/display/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<div>
+	 	<form action="" method="post">
+	 		<label for="login_email">Email: </label>
+	 			<input name="login_email" type="email" />
+
+	 		<label for="login_email" >Password: </label>
+	 			<input name="login_pass" type="password" />
+			<button type="submit" name="login_submit">Submit</button>
+	 	</form>
+	</div>
+</body>
+</html>
+
+
