5c3a4b7f1a95c99147207e58caff1f74cc3f842a
NourEldin275 <noureldin.ali.m@gmail.com>
2015-05-10 10:38:48 +0200

    Refined the login page and added jQuery library and style sheet file.

--- View/display/login.php
+++ View/display/login.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
 	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div>
-	 	<form action="" method="post">
-	 		<label for="login_email">Email: </label>
-	 			<input name="login_email" type="email" />
+	<div id="login-form-container">
+		<h1>Reminva Login</h1>
+	 	<form action="" method="post" id="login_form">
+	 		<label for="login_email">Email: </label><br>
+	 			<input name="login_email" type="email" id="login-form-field"/><br>
 
-	 		<label for="login_email" >Password: </label>
-	 			<input name="login_pass" type="password" />
+	 		<label for="login_email" >Password: </label><br>
+	 			<input name="login_pass" type="password" id="login-form-field"/><br>
 			<button type="submit" name="login_submit">Submit</button>
 	 	</form>
 	</div>
+	<script type="text/javascript" src="../../resources/lib/JS/jquery-2.1.4.js"></script>
+	<!--<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.js"></script>-->
+	<script type="text/javascript" src="../js/reminvaJS.js"></script>
 </body>
 </html>
 
